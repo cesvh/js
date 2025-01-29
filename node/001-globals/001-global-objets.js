00faca3bbd425cd6e00dab4da2c2c48510780815
cesvh <cesar.vega.hdez@gmail.com>
2025-01-28 23:11:07 -0600

    fix: global objets

--- node/001-globals/001-global-objets.js
+++ node/001-globals/001-global-objets.js
@@ -1,5 +1,6 @@
 console.log(__dirname);
 console.log(__filename);
+console.log(module);
 
 /*
 
@@ -7,6 +8,25 @@ PS C:\Users\cesvh\dev\github\js\node\001-globals>
 PS C:\Users\cesvh\dev\github\js\node\001-globals> node .\001-global-objets.js
 C:\Users\cesvh\dev\github\js\node\001-globals
 C:\Users\cesvh\dev\github\js\node\001-globals\001-global-objets.js
-PS C:\Users\cesvh\dev\github\js\node\001-globals> 
+{
+  id: '.',
+  path: 'C:\\Users\\cesvh\\dev\\github\\js\\node\\001-globals',
+  exports: {},
+  filename: 'C:\\Users\\cesvh\\dev\\github\\js\\node\\001-globals\\001-global-objets.js',
+  loaded: false,
+  children: [],
+  paths: [
+    'C:\\Users\\cesvh\\dev\\github\\js\\node\\001-globals\\node_modules',
+    'C:\\Users\\cesvh\\dev\\github\\js\\node\\node_modules',
+    'C:\\Users\\cesvh\\dev\\github\\js\\node_modules',
+    'C:\\Users\\cesvh\\dev\\github\\node_modules',
+    'C:\\Users\\cesvh\\dev\\node_modules',
+    'C:\\Users\\cesvh\\node_modules',
+    'C:\\Users\\node_modules',
+    'C:\\node_modules'
+  ]
+}
+PS C:\Users\cesvh\dev\github\js\node\001-globals>
+PS C:\Users\cesvh\dev\github\js\node\001-globals>
 
 */
