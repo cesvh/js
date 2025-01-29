01dd8963e4112f8dbb2e5124eceed9fe4e02b61a
cesvh <cesar.vega.hdez@gmail.com>
2025-01-28 23:09:26 -0600

    feat: global objets en node

---
+++ node/001-globals/001-global-objets.js
@@ -0,0 +1,12 @@
+console.log(__dirname);
+console.log(__filename);
+
+/*
+
+PS C:\Users\cesvh\dev\github\js\node\001-globals> 
+PS C:\Users\cesvh\dev\github\js\node\001-globals> node .\001-global-objets.js
+C:\Users\cesvh\dev\github\js\node\001-globals
+C:\Users\cesvh\dev\github\js\node\001-globals\001-global-objets.js
+PS C:\Users\cesvh\dev\github\js\node\001-globals> 
+
+*/
