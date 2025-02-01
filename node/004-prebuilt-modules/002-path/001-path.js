835853f2d6cca02fffe074eb9e8e264b7826c22d
cesvh <cesar.vega.hdez@gmail.com>
2025-01-31 20:38:48 -0600

    feat: path

---
+++ node/004-prebuilt-modules/002-path/001-path.js
@@ -0,0 +1,19 @@
+// Module PATH
+// windows: C:\\Users\\CES\\
+// linux: home/ces/
+const path = require('path');
+
+// console.log("--------------------------------------path.join");
+// console.log(path.join('public', 'dist', 'styles', 'main.css'));
+
+const filepath = path.join('public', 'dist', 'styles', 'main.css');
+
+
+// console.log("--------------------------------------path.dirname");
+// console.log(path.dirname(filepath));
+// console.log("--------------------------------------path.basename");
+// console.log(path.basename(filepath));
+// console.log("--------------------------------------path.parse");
+// console.log(path.parse(filepath));
+// console.log("--------------------------------------path.resolve");
+// console.log(path.resolve('module'));
