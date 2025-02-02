922688ce4f54d3995340f4d50970798deb9fcef0
cesvh <cesar.vega.hdez@gmail.com>
2025-02-01 18:29:41 -0600

    feat: npm
    install and uninstall

---
+++ node/005-npm/001-npm/index.js
@@ -0,0 +1,102 @@
+console.log('...');
+
+/*
+
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> tree
+Listado de rutas de carpetas para el volumen Windows
+El número de serie del volumen es DA4A-FA86
+C:.
+No existe ninguna subcarpeta
+
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\node\005-npm\001-npm
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a---       01/02/2025 03:38 p. m.              0 index.js
+
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> npm install express
+
+added 69 packages in 3s
+
+14 packages are looking for funding
+  run `npm fund` for details
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\node\005-npm\001-npm
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       01/02/2025 03:49 p. m.                node_modules
+-a---       01/02/2025 03:38 p. m.              0 index.js
+-a---       01/02/2025 03:49 p. m.          27778 package-lock.json
+-a---       01/02/2025 03:49 p. m.             53 package.json
+
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> cat .\package.json
+{
+  "dependencies": {
+    "express": "^4.21.2"
+  }
+}
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> npm install mysql
+
+added 12 packages, and audited 82 packages in 2s
+
+14 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> cat .\package.json
+{
+  "dependencies": {
+    "express": "^4.21.2",
+    "mysql": "^2.18.1"
+  }
+}
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> npm install bootstrap
+
+added 2 packages, and audited 84 packages in 2s
+
+16 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> cat .\package.json
+{
+  "dependencies": {
+    "bootstrap": "^5.3.3",
+    "express": "^4.21.2",
+    "mysql": "^2.18.1"
+  }
+}
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> npm uninstall bootstrap #npm remove bootstrap
+
+removed 2 packages, and audited 82 packages in 811ms
+
+14 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm> cat .\package.json
+{
+  "dependencies": {
+    "express": "^4.21.2",
+    "mysql": "^2.18.1"
+  }
+}
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+PS C:\Users\cesvh\dev\github\js\node\005-npm\001-npm>
+
+*/
