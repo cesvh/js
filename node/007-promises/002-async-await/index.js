be71cfba47099c0564c1fd6052c3118b470a1ead
cesvh <cesar.vega.hdez@gmail.com>
2025-02-04 11:34:22 -0600

    fix: asincronismo
    async await

--- node/007-promises/002-async-await/index.js
+++ node/007-promises/002-async-await/index.js
@@ -1,5 +1,6 @@
 const { readFile } = require('fs');
 
+// Código asíncrono: Se ejecuta en segundo plano
 const getText = (pathFile) => {
     return new Promise((resolve, reject) => {
         readFile(pathFile, 'utf-8', (err, data) => {
@@ -11,8 +12,10 @@ const getText = (pathFile) => {
     });
 };
 
+// async: especifica que dentro tiene código asíncrono
 const readText = async () => {
     try{
+        // await: especifica que espera una promesa
         const result1 = await getText('./datas/autors.txt');
         const result2 = await getText('./datas/books.txt');
         const result3 = await getText('./datas/topics.txt');
@@ -26,3 +29,22 @@ const readText = async () => {
 };
 
 readText();
+
+/*
+
+PS C:\Users\cesvh\dev\github\js\node\007-promises\002-async-await>
+PS C:\Users\cesvh\dev\github\js\node\007-promises\002-async-await>
+PS C:\Users\cesvh\dev\github\js\node\007-promises\002-async-await> node .\index.js
+Cixin Liu
+Isaac Asimov
+
+El problema de los 3 cuerpos
+El bosque oscuro
+
+sci-fi
+cyberpunk
+
+PS C:\Users\cesvh\dev\github\js\node\007-promises\002-async-await>
+PS C:\Users\cesvh\dev\github\js\node\007-promises\002-async-await>
+
+*/
