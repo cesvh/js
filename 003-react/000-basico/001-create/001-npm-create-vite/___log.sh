412ca150ad964ef1fc92754df0df0a881308b260
cesvh <cesar.vega.hdez@gmail.com>
2025-02-11 12:01:57 -0600

    feat: create vite app
    npm create vite

---
+++ 003-react/000-basico/001-create/001-npm-create-vite/___log.sh
@@ -0,0 +1,48 @@
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create> ls
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create> npm create vite
+√ Project name: ... 001-npm-create-vite
+√ Select a framework: » React
+√ Select a variant: » JavaScript
+
+Scaffolding project in C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite...
+
+Done. Now run:
+
+  cd 001-npm-create-vite
+  npm install
+  npm run dev
+
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create> cd .\001-npm-create-vite\
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite> npm install
+npm WARN deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+npm WARN deprecated @humanwhocodes/config-array@0.13.0: Use @eslint/config-array instead
+npm WARN deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
+npm WARN deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+npm WARN deprecated @humanwhocodes/object-schema@2.0.3: Use @eslint/object-schema instead
+npm WARN deprecated eslint@8.57.1: This version is no longer supported. Please see https://eslint.org/version-support for other options.
+
+added 273 packages, and audited 274 packages in 20s
+
+109 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite> npm run dev
+
+> 001-npm-create-vite@0.0.0 dev
+> vite
+
+
+  VITE v4.5.9  ready in 345 ms
+
+  ➜  Local:   http://localhost:5173/
+  ➜  Network: use --host to expose
+  ➜  press h to show help
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\001-npm-create-vite>
