cf79792adbd61ad3d2e374ff38f9d509073f1b2a
cesvh <cesar.vega.hdez@gmail.com>
2025-02-13 17:34:14 -0600

    feat: task app create

---
+++ 003-react/100-projects/001-tasks/___logs.sh
@@ -0,0 +1,46 @@
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects> npm create vite@latest
+√ Project name: ... 001-tasks
+√ Select a framework: » React
+√ Select a variant: » JavaScript
+
+Scaffolding project in C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks...
+
+Done. Now run:
+
+  cd 001-tasks
+  npm install
+  npm run dev
+
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects> cd .\001-tasks\
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks> npm install
+npm WARN EBADENGINE Unsupported engine {
+npm WARN EBADENGINE   package: 'vite@6.1.0',
+npm WARN EBADENGINE   required: { node: '^18.0.0 || ^20.0.0 || >=22.0.0' },
+npm WARN EBADENGINE   current: { node: 'v21.1.0', npm: '10.2.3' }
+npm WARN EBADENGINE }
+
+added 260 packages, and audited 261 packages in 18s
+
+108 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks> npm run dev
+
+> 001-tasks@0.0.0 dev
+> vite
+
+
+  VITE v6.1.0  ready in 261 ms
+
+  ➜  Local:   http://localhost:5173/
+  ➜  Network: use --host to expose
+  ➜  press h + enter to show help
+^C^C¿Desea terminar el trabajo por lotes (S/N)?
+^C
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\001-tasks>
