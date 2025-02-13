70f598e4c0f205640b4c9a03901befcb4fb777f3
cesvh <cesar.vega.hdez@gmail.com>
2025-02-13 16:29:59 -0600

    feat: npm create vite

---
+++ 003-react/000-basico/001-create/100-create-vite/___logs.sh
@@ -0,0 +1,72 @@
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\004-hooks\001-use-effect>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\004-hooks\001-use-effect> cd ..
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\004-hooks>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\004-hooks> cd ..
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\003-react\000-basico
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       11/02/2025 12:09 p. m.                001-create
+d----       12/02/2025 05:01 p. m.                002-componentes
+d----       12/02/2025 05:31 p. m.                003-apis
+d----       13/02/2025 12:29 p. m.                004-hooks
+
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico> cd .\001-create\
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       11/02/2025 11:06 a. m.                001-npm-create-vite
+d----       11/02/2025 12:17 p. m.                002-hola-mundo
+
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create> npm create vite
+√ Project name: ... 100-create-vite
+√ Select a framework: » React
+√ Select a variant: » JavaScript
+
+Scaffolding project in C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite...
+
+Done. Now run:
+
+  cd 100-create-vite
+  npm install
+  npm run dev
+
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create> cd .\100-create-vite\
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite> npm install
+npm WARN deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+npm WARN deprecated @humanwhocodes/config-array@0.13.0: Use @eslint/config-array instead
+npm WARN deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
+npm WARN deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+npm WARN deprecated @humanwhocodes/object-schema@2.0.3: Use @eslint/object-schema instead
+npm WARN deprecated eslint@8.57.1: This version is no longer supported. Please see https://eslint.org/version-support for other options.
+
+added 273 packages, and audited 274 packages in 22s
+
+109 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite> npm run dev
+
+> 100-create-vite@0.0.0 dev
+> vite
+
+
+  VITE v4.5.9  ready in 279 ms
+
+  ➜  Local:   http://localhost:5173/
+  ➜  Network: use --host to expose
+  ➜  press h to show help
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite>
+PS C:\Users\cesvh\dev\github\js\003-react\000-basico\001-create\100-create-vite>
