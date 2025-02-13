e96be8e0789c7fc62dcfe434c87e146609f6d46f
cesvh <cesar.vega.hdez@gmail.com>
2025-02-12 19:07:55 -0600

    fix: Apis - fetch
    fetch and map

--- 003-react/000-basico/003-apis/001-fetch/src/Components/Posts.jsx
+++ 003-react/000-basico/003-apis/001-fetch/src/Components/Posts.jsx
@@ -2,7 +2,12 @@ export function Button() {
     const GetPosts = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
-        .then(datas => console.log(datas));
+        .then(datas => {
+            let posts = datas.map(( item, index ) => {
+                return item.title.toUpperCase();
+            });
+            console.log(posts);
+        });
     };
     return <>
         <button onClick={GetPosts}>Pedir Datos en consola</button>
