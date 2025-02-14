a76acbfaf8cb9b5f481a102a4d44ad68b8335d12
cesvh <cesar.vega.hdez@gmail.com>
2025-02-14 00:04:46 -0600

    feat: task app 2
    Agregando y eliminando tareas

--- 003-react/100-projects/001-tasks/src/App.jsx
+++ 003-react/100-projects/001-tasks/src/App.jsx
@@ -1,40 +1,56 @@
 import { useState } from 'react'
 import { tasks as myDatas } from './datas/tasks';
 import { useEffect } from 'react';
-console.log(myDatas);
 
 function App() {
-    // Al no inicializar: Uncaught TypeError: myTasks is undefined
     const [myTasks, setMyTasksList] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    
     useEffect(() => {
-        console.log("useEffect - []");
         setMyTasksList(myDatas);
-    }, []); // Se ejecuta un sola vez al inicio del render
-    if(myTasks.length === 0) {
-        return <h3>No hay tareas</h3>
-    }
+    }, []);
+
+    const addTask = (e) => {
+        e.preventDefault();
+        setMyTasksList([...myTasks, { title, description, id: myTasks.length }]);
+        setTitle("");
+        setDescription("");
+    };
+
+    const deleteTask = (id) => {
+        const deleteTask = myTasks.filter( task => task.id !== id);
+        setMyTasksList(deleteTask);
+    };
+
+    if(myTasks.length === 0) return <h3>No hay tareas</h3>;
+
     return (
     <>
-        <form>
+        <form onSubmit={addTask}>
             <input
                 id="title"
                 name="title"
                 placeholder="Escribe la tarea"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
                 autoFocus />
             <br /><br />
             <input
                 id="description"
                 name="description"
-                placeholder="Describe la tarea" />
+                placeholder="Describe la tarea"
+                onChange={(e) => setDescription(e.target.value)}
+                value={description} />
             <br /><br />
             <button>Guardar Tarea</button>
         </form>
         <div>
             { myTasks.map((row) => (
                 <div key={row.id} >
-                    <h3>{row.title}</h3>
+                    <h3>{row.id} - {row.title}</h3>
                     <p>{row.description}</p>
-                    <button>
+                    <button onClick={ () => deleteTask(row.id) }>
                         Eliminar Tarea
                     </button>
                 </div>
