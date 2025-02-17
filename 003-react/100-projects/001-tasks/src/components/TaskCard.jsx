64fd546bfe8c48cd4cbffca9fbaf3285b4e9b735
cesvh <cesar.vega.hdez@gmail.com>
2025-02-17 11:36:32 -0600

    feat: task app 5
    Componente card

--- 003-react/100-projects/001-tasks/src/components/TaskCard.jsx
+++ 003-react/100-projects/001-tasks/src/components/TaskCard.jsx
@@ -1,14 +1,11 @@
-function TaskCard({ row, deleteTask }) {
-    // function eliminarTarea(){
-    //     deleteTask(row.id)
-    // }
+function TaskCard({ task, deleteTask }) {
     return (
         <div>
-            <h1>{row.title}</h1>
-            <p>{row.description}</p>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
             <button 
-                onClick={() => deleteTask(row.id)} >
-                Eliminar Tarea
+                onClick={() => deleteTask(task.id)} >
+                Eliminar Tarea...
             </button>
         </div>
     );
--- 003-react/100-projects/001-tasks/src/components/TaskList.jsx
+++ 003-react/100-projects/001-tasks/src/components/TaskList.jsx
@@ -1,21 +1,14 @@
-// import TaskCard from "./TaskCard";
+import TaskCard from "./TaskCard";
 
 function TaskList({ myTasks, deleteTask }) {
     if(myTasks.length === 0) return <h3>No hay tareas</h3>;
     return (
         <div>
             { myTasks.map((row) => (
-                <div key={row.id} >
-                    <h3>{row.id} - {row.title}</h3>
-                    <p>{row.description}</p>
-                    <button onClick={ () => deleteTask(row.id) }>
-                        Eliminar Tarea
-                    </button>
-                </div>
-                // <TaskCard 
-                //     key={row.id} 
-                //     task={row} 
-                //     deleteTask={deleteTask} />
+                <TaskCard 
+                    key={row.id} 
+                    task={row} 
+                    deleteTask={deleteTask} />
             ))}
         </div>
     );
