894247d847b6b25b678dccae1edf0d9672c464b6
cesvh <cesar.vega.hdez@gmail.com>
2025-02-18 16:58:24 -0600

    feat: Nextjs - rutas
    Rutas en next.js

---
+++ 004-nextjs/000-basic/002-ruotes/___logs.sh
@@ -0,0 +1,68 @@
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app> cd ..
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\004-nextjs\000-basic
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       18/02/2025 04:06 p. m.                001-create-next-app
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> npx create-next-app 002-ruotes
+√ Would you like to use TypeScript? ... No / Yes
+√ Would you like to use ESLint? ... No / Yes
+√ Would you like to use Tailwind CSS? ... No / Yes
+√ Would you like your code inside a `src/` directory? ... No / Yes
+√ Would you like to use App Router? (recommended) ... No / Yes
+√ Would you like to use Turbopack for `next dev`? ... No / Yes
+√ Would you like to customize the import alias (`@/*` by default)? ... No / Yes
+Creating a new Next.js app in C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\002-ruotes.
+
+Using npm.
+
+Initializing project with template: app
+
+
+Installing dependencies:
+- react
+- react-dom
+- next
+
+Installing devDependencies:
+- eslint
+- eslint-config-next
+- @eslint/eslintrc
+
+
+added 296 packages, and audited 297 packages in 44s
+
+125 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+Success! Created 002-ruotes at C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\002-ruotes
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> cd .\002-ruotes\
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\002-ruotes>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\002-ruotes>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\002-ruotes> npm run dev
+
+> 002-ruotes@0.1.0 dev
+> next dev
+
+   ▲ Next.js 15.1.7
+   - Local:        http://localhost:3000
+   - Network:      http://192.168.1.66:3000
+
+ ✓ Starting...
+ ✓ Ready in 1678ms
+(node:13176) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+(Use `node --trace-deprecation ...` to show where the warning was created)
+ ○ Compiling / ...
+ ✓ Compiled / in 4.8s (674 modules)
+ GET / 200 in 5380ms
