15946ab476de0042c4da6067dbcf13f5a9183f51
cesvh <cesar.vega.hdez@gmail.com>
2025-02-18 15:58:56 -0600

    feat: Nextjs
    npx create-next-app projectName

---
+++ 004-nextjs/000-basic/001-create-next-app/___logs.sh
@@ -0,0 +1,69 @@
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> npx create-next-app 001-create-next-app
+Need to install the following packages:
+create-next-app@15.1.7
+Ok to proceed? (y)
+√ Would you like to use TypeScript? ... No / Yes
+√ Would you like to use ESLint? ... No / Yes
+√ Would you like to use Tailwind CSS? ... No / Yes
+√ Would you like your code inside a `src/` directory? ... No / Yes
+√ Would you like to use App Router? (recommended) ... No / Yes
+√ Would you like to use Turbopack for `next dev`? ... No / Yes
+√ Would you like to customize the import alias (`@/*` by default)? ... No / Yes
+Creating a new Next.js app in C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app.
+
+Using npm.
+
+Initializing project with template: app
+
+
+Installing dependencies:
+- react
+- react-dom
+- next
+
+Installing devDependencies:
+- eslint
+- eslint-config-next
+- @eslint/eslintrc
+
+
+added 296 packages, and audited 297 packages in 54s
+
+125 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+Success! Created 001-create-next-app at C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> cd .\001-create-next-app\
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app> npm run dev
+
+> 001-create-next-app@0.1.0 dev
+> next dev
+
+   ▲ Next.js 15.1.7
+   - Local:        http://localhost:3000
+   - Network:      http://192.168.1.66:3000
+
+ ✓ Starting...
+Attention: Next.js now collects completely anonymous telemetry regarding usage.
+This information is used to shape Next.js' roadmap and prioritize features.
+You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
+https://nextjs.org/telemetry
+
+ ✓ Ready in 1756ms
+ ○ Compiling / ...
+(node:4828) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+(Use `node --trace-deprecation ...` to show where the warning was created)
+ ✓ Compiled / in 5.2s (674 modules)
+ GET / 200 in 5807ms
+ ✓ Compiled /favicon.ico in 502ms (354 modules)
+ GET /favicon.ico 200 in 607ms
+
+¿Desea terminar el trabajo por lotes (S/N)?
+^C
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\001-create-next-app>
