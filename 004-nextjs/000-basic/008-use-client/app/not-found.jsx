87dcd5b67c1a4afa713832afe439357afea44b1c
cesvh <cesar.vega.hdez@gmail.com>
2025-02-19 17:30:07 -0600

    feat: Nextjs - use client

---
+++ 004-nextjs/000-basic/008-use-client/app/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function PageNotFound() {
+    return <>
+        <section>
+            <h1>4004</h1>
+            <p>Página no econtrada</p>
+            <Link href="/">Ir a inicio</Link>
+        </section>
+    </>;
+};
