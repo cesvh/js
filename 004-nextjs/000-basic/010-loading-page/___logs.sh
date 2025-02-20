29f19ea7a2765ce2a9dea0971ca8d032bed7f3bc
cesvh <cesar.vega.hdez@gmail.com>
2025-02-19 19:16:37 -0600

    feat: Nextjs - loading page

---
+++ 004-nextjs/000-basic/010-loading-page/___logs.sh
@@ -0,0 +1,54 @@
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> npx create-next-app 010-loading-page
+√ Would you like to use TypeScript? ... No / Yes
+√ Would you like to use ESLint? ... No / Yes
+√ Would you like to use Tailwind CSS? ... No / Yes
+√ Would you like your code inside a `src/` directory? ... No / Yes
+√ Would you like to use App Router? (recommended) ... No / Yes
+√ Would you like to use Turbopack for `next dev`? ... No / Yes
+√ Would you like to customize the import alias (`@/*` by default)? ... No / Yes
+Creating a new Next.js app in C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\010-loading-page.
+
+Using npm.
+
+Initializing project with template: app
+
+
+Installing dependencies:
+- react
+- react-dom
+- next
+
+Installing devDependencies:
+- eslint
+- eslint-config-next
+- @eslint/eslintrc
+
+
+added 296 packages, and audited 297 packages in 40s
+
+125 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+Success! Created 010-loading-page at C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\010-loading-page
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> cd .\010-loading-page\
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\010-loading-page>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\010-loading-page> npm run dev
+
+> 010-loading-page@0.1.0 dev
+> next dev
+
+   ▲ Next.js 15.1.7
+   - Local:        http://localhost:3000
+   - Network:      http://192.168.1.66:3000
+
+ ✓ Starting...
+ ✓ Ready in 2.1s
+(node:22372) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+(Use `node --trace-deprecation ...` to show where the warning was created)
+ ○ Compiling / ...
+ ✓ Compiled / in 5.4s (600 modules)
+ GET / 200 in 5871ms
