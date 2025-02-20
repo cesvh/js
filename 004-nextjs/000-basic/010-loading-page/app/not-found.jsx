cf3f282481500d0eeee24c002f8550aec9bef270
cesvh <cesar.vega.hdez@gmail.com>
2025-02-20 00:56:29 -0600

    fix: Code 404
    Not Found

--- 004-nextjs/000-basic/010-loading-page/app/not-found.jsx
+++ 004-nextjs/000-basic/010-loading-page/app/not-found.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PageNotFound() {
     return <>
         <section>
-            <h1>4004</h1>
+            <h1>404</h1>
             <p>Página no econtrada</p>
             <Link href="/">Ir a inicio</Link>
         </section>
