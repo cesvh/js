9dc55ec4c7105f45566880200f69e55839f331d3
cesvh <cesar.vega.hdez@gmail.com>
2025-02-20 20:43:22 -0600

    feat: nextjs first app create

---
+++ 004-nextjs/100-projects/001-first-project/___logs.sh
@@ -0,0 +1,41 @@
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects> npx create-next-app 001-first-project
+√ Would you like to use TypeScript? ... No / Yes #No
+√ Would you like to use ESLint? ... No / Yes #Yes
+√ Would you like to use Tailwind CSS? ... No / Yes #Yes
+√ Would you like your code inside a `src/` directory? ... No / Yes #No
+√ Would you like to use App Router? (recommended) ... No / Yes #Yes
+√ Would you like to use Turbopack for `next dev`? ... No / Yes #No
+√ Would you like to customize the import alias (`@/*` by default)? ... No / Yes #No
+Creating a new Next.js app in C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\001-first-project.
+
+Using npm.
+
+Initializing project with template: app-tw
+
+
+Installing dependencies:
+- react
+- react-dom
+- next
+
+Installing devDependencies:
+- postcss
+- tailwindcss
+- eslint
+- eslint-config-next
+- @eslint/eslintrc
+
+
+added 370 packages, and audited 371 packages in 44s
+
+144 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+Success! Created 001-first-project at C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\001-first-project
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects> cd .\001-first-project\
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\001-first-project>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\001-first-project>
