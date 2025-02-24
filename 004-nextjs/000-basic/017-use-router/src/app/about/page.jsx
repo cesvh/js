7d9c499c550bd13e787a97bf987a69fe48539bf2
cesvh <cesar.vega.hdez@gmail.com>
2025-02-24 16:55:35 -0600

    feat: nextjs-useRouter base

---
+++ 004-nextjs/000-basic/017-use-router/src/app/about/page.jsx
@@ -0,0 +1,12 @@
+// import AboutContent from "../../components/AboutContent";
+import AboutContent from "@/components/AboutContent";
+
+function About(){
+    return <>
+        <h1>Acerca de</h1>
+        <AboutContent />
+    </>
+}
+
+export default About;
+
