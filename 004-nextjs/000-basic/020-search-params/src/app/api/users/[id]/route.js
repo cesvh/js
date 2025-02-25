ababf5f851c7c9b19961c256f936bbf824d6a004
cesvh <cesar.vega.hdez@gmail.com>
2025-02-25 16:57:33 -0600

    feat: nextjs-search params 2
    Api usando search params

--- 004-nextjs/000-basic/020-search-params/src/app/api/users/[id]/route.js
+++ 004-nextjs/000-basic/020-search-params/src/app/api/users/[id]/route.js
@@ -4,7 +4,12 @@ export async function GET( request, { params } ){
     // console.log(await params);
     // console.log(users_json);
     const { id } = await params;
-    console.log(id);
+    console.log(id); // 2
+    // console.log(new URL(request.url).searchParams); // URLSearchParams { 'name' => 'ces', 'team' => 'lux' }
+    const { searchParams } = new URL(request.url); // URLSearchParams { 'name' => 'ces', 'team' => 'lux' }
+    // console.log(searchParams);
+    console.log(searchParams.get("name")); // ces
+    console.log(searchParams.get("team")); // lux
     const users_res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
     const users_json = await users_res.json();
     return NextResponse.json(users_json);
