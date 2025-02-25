9f2d5b13e43c34db063b495e742329874e3a087c
cesvh <cesar.vega.hdez@gmail.com>
2025-02-25 16:45:18 -0600

    feat: nextjs-search params 1
    Api usando search params

---
+++ 004-nextjs/000-basic/020-search-params/___logs.sh
@@ -0,0 +1,48 @@
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> npx create-next-app 020-search-params
+√ Would you like to use TypeScript? ... No / Yes
+√ Would you like to use ESLint? ... No / Yes
+√ Would you like to use Tailwind CSS? ... No / Yes
+√ Would you like your code inside a `src/` directory? ... No / Yes
+√ Would you like to use App Router? (recommended) ... No / Yes
+√ Would you like to use Turbopack for `next dev`? ... No / Yes
+√ Would you like to customize the import alias (`@/*` by default)? ... No / Yes
+Creating a new Next.js app in C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\020-search-params.
+
+Using npm.
+
+Initializing project with template: app
+
+
+Installing dependencies:
+- react
+- react-dom
+- next
+
+Installing devDependencies:
+- eslint
+- eslint-config-next
+- @eslint/eslintrc
+
+
+added 296 packages, and audited 297 packages in 44s
+
+125 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+Success! Created 020-search-params at C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\020-search-params
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic> cd .\020-search-params\
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\020-search-params>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\020-search-params> npm install tailwindcss @tailwindcss/postcss postcss
+
+added 11 packages, changed 1 package, and audited 308 packages in 3s
+
+129 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\020-search-params>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\000-basic\020-search-params>
