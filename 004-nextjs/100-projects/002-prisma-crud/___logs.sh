a606df0d2fb39bf216bc6d597baa94d169845bd2
cesvh <cesar.vega.hdez@gmail.com>
2025-02-26 10:23:50 -0600

    feat: nextjs-crud prisma 1
    Inicio del proyecto de crud usando prisma

---
+++ 004-nextjs/100-projects/002-prisma-crud/___logs.sh
@@ -0,0 +1,41 @@
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects> npx create-next-app 002-prisma-crud
+√ Would you like to use TypeScript? ... No / Yes
+√ Would you like to use ESLint? ... No / Yes
+√ Would you like to use Tailwind CSS? ... No / Yes
+√ Would you like your code inside a `src/` directory? ... No / Yes
+√ Would you like to use App Router? (recommended) ... No / Yes
+√ Would you like to use Turbopack for `next dev`? ... No / Yes
+√ Would you like to customize the import alias (`@/*` by default)? ... No / Yes
+Creating a new Next.js app in C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\002-prisma-crud.
+
+Using npm.
+
+Initializing project with template: app-tw
+
+
+Installing dependencies:
+- react
+- react-dom
+- next
+
+Installing devDependencies:
+- postcss
+- tailwindcss
+- eslint
+- eslint-config-next
+- @eslint/eslintrc
+
+
+added 370 packages, and audited 371 packages in 43s
+
+144 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+Success! Created 002-prisma-crud at C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\002-prisma-crud
+
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects> cd .\002-prisma-crud\
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\002-prisma-crud>
+PS C:\Users\cesvh\dev\github\js\004-nextjs\100-projects\002-prisma-crud>
