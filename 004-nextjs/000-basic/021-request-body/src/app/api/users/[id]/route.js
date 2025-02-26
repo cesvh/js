5b7974dc1dc8ca10e55f9fdea2dfdc45a31d5376
cesvh <cesar.vega.hdez@gmail.com>
2025-02-25 21:55:55 -0600

    feat: nextjs-request body 3 .env
    Api usando request body {} uso de datos del archivo .env

--- 004-nextjs/000-basic/021-request-body/src/app/api/users/[id]/route.js
+++ 004-nextjs/000-basic/021-request-body/src/app/api/users/[id]/route.js
@@ -10,6 +10,9 @@ export async function GET( request, { params } ){
     // console.log(searchParams);
     console.log(searchParams.get("name")); // ces
     console.log(searchParams.get("team")); // lux
+    // Uso de datos del archivo .env
+    console.log(process.env.TOKEN); // Qpa312888dhgdj3h4h69ddf44577fdfd
+    console.log(process.env.SECRET_KEY); // T545456456guytyuvgvfdc5456JQIDJET557UQJED44
     const users_res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
     const users_json = await users_res.json();
     return NextResponse.json(users_json);
