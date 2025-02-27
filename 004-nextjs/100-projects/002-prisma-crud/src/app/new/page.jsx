816c39fd948f5bf93d3f55be5696c916b4aebb21
cesvh <cesar.vega.hdez@gmail.com>
2025-02-26 18:28:41 -0600

    feat: nextjs-crud prisma 4
    Agregar tarea desde el front

--- 004-nextjs/100-projects/002-prisma-crud/src/app/new/page.jsx
+++ 004-nextjs/100-projects/002-prisma-crud/src/app/new/page.jsx
@@ -1,5 +1,42 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 function NewPage(){
-    return <div>Formulario de tareas</div>
+    const router = useRouter();
+    const onHandlerSubmit = async (e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const description = e.target.description.value;
+        const res = await fetch(`/api/tasks`, {
+            method: "POST",
+            body: JSON.stringify({title, description}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json();
+        console.log(data);
+        router.push("/");
+    };
+
+    return <div className="h-screen flex justify-center items-center">
+        <form className="bg-slate-800 p-10 w-1/4" onSubmit={onHandlerSubmit}>
+            <label htmlFor="title" 
+                className="font-bold text-sm">Título de la tarea</label>
+            <input type="text"
+                id="title"
+                className="border border-gray-400 p-2 mb-4 w-full text-black"
+                placeholder="Tarea a realizar" />
+            <label htmlFor="description" 
+                className="font-bold text-sm">Descripción de la tarea</label>
+            <textarea rows="3" 
+                id="description"
+                className="border border-gray-400 p-2 mb-4 w-full text-black"
+                placeholder="Describe tu tarea" ></textarea>
+            <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Crear</button>
+        </form>
+    </div>
 }
 
 export default NewPage;
