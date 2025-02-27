db21f28e965b4cf448f9e9c5fb0befe6d9761232
cesvh <cesar.vega.hdez@gmail.com>
2025-02-27 00:14:41 -0600

    feat: nextjs-crud prisma 5
    Listado de tareas del lado del servidor

--- 004-nextjs/100-projects/002-prisma-crud/src/app/page.jsx
+++ 004-nextjs/100-projects/002-prisma-crud/src/app/page.jsx
@@ -1,5 +1,35 @@
-function HomePage(){
-    return <div>Tareas</div>;
+import { prisma } from "@/libs/prisma";
+
+async function loadTasks(){
+    // Formas de obtener datos
+    // 1- Consumir api con /api/tasks
+    // const res = await fetch(`http://localhost:3000/api/tasks`);
+    // const data = await res.json();
+    // 2- Obtener de la db con prisma
+    const tasks = await prisma.task.findMany();
+    return tasks;
+}
+
+async function HomePage(){
+    const tasks = await loadTasks();
+    // console.log(tasks);
+    return <section className="container mx-auto">
+        <div className="grid grid-cols-3 gap-3 mt-10">
+            {tasks.map((task) => (
+                <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
+                    <h3 className="font-bold text-2xl mb-2">
+                        {task.title}
+                    </h3>
+                    <p>
+                        {task.description}
+                    </p>
+                    <p>
+                        {new Date(task.creationAt).toLocaleDateString()}
+                    </p>
+                </div>
+            ))}
+        </div>
+    </section>;
 }
 
 export default HomePage;
