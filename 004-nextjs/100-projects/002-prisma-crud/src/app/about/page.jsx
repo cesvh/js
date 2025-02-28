33dab0c19302b982cfa0d7229fbf9fb965378ea5
cesvh <cesar.vega.hdez@gmail.com>
2025-02-27 18:40:01 -0600

    feat: nextjs-crud prisma 10
    Navegación y otros detalles

--- 004-nextjs/100-projects/002-prisma-crud/src/app/about/page.jsx
+++ 004-nextjs/100-projects/002-prisma-crud/src/app/about/page.jsx
@@ -1,5 +1,12 @@
 function AboutPage(){
-    return <div>About page...</div>
+    return (
+        <section className="container mx-auto">
+            <h3 className="text-4xl font-bold my-5">Acerca de</h3>
+            <p>
+                Entre el tercer y cuarto milenio antes de nuestra era, el fondo del golfo Pérsico se encontraba ciento cincuenta kilómetros más al norte que en la actualidad. El Tigris y el Éufrates, que ahora confluyen para formar el Shatt-el-Arab, tenían entonces sus propias desembocaduras. Cuando la civilización nació en Mesopotamia, surgida del pueblo sumerio, las primeras ciudades se levantaron a orillas de los ríos y en el litoral del golfo. Hacia el tercer milenio, un segundo pueblo del norte de Mesopotamia, de origen semita, los acadios, se mezcló con los súmeros y, como ocurre a menudo en la historia, los conquistadores fueron a su vez conquistados por el espíritu superior de los vencidos. Los  acadios adoptaron el arte, la ciencia, las instituciones y la escritura del país invadido. Pero como aquellos semitas hablaban un lenguaje más perfecto que el de los súmeros, la lengua acadia se impuso, aunque el idioma sumerio continuó empleándose para el culto religioso y como vehículo cultural.
+            </p>
+        </section>
+    );
 }
 
 export default AboutPage;
