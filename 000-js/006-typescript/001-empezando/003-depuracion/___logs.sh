2f76ad9f5600bcd0fada395f2dc8d9782c70050c
cesvh <cesar.vega.hdez@gmail.com>
2025-03-05 10:45:21 -0600

    feat: ts - depuración inicial

---
+++ 000-js/006-typescript/001-empezando/003-depuracion/___logs.sh
@@ -0,0 +1,27 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion> ls
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a---       05/03/2025 10:38 a. m.          12926 tsconfig.json
+
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
