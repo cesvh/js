347866411edca4d4c4f4bfe2522b47a341b72e01
cesvh <cesar.vega.hdez@gmail.com>
2025-03-04 18:09:24 -0600

    feat: js-ts: tsc ---init 0

---
+++ 000-js/006-typescript/001-empezando/002-tsc-init/___logs.sh
@@ -0,0 +1,16 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\002-tsc-init>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\002-tsc-init> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\002-tsc-init>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\002-tsc-init>
