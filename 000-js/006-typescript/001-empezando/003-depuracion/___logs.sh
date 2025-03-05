b4b0fdca9e6152eb065647182d4500418c14a752
cesvh <cesar.vega.hdez@gmail.com>
2025-03-05 11:28:41 -0600

    feat: ts - depuración 1
    "sourceMap": true
    
    se agrega en .vscode->launch.json -> "preLaunchTask": "tsc: build - tsconfig.json",

--- 000-js/006-typescript/001-empezando/003-depuracion/___logs.sh
+++ 000-js/006-typescript/001-empezando/003-depuracion/___logs.sh
@@ -24,4 +24,11 @@ Mode                 LastWriteTime         Length Name
 -a---       05/03/2025 10:38 a. m.          12926 tsconfig.json
 
 PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion> ts-node --version
+v10.9.2
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion> ts-node .\src\index.ts
+César
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion> tsc
 PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\001-empezando\003-depuracion>
---
+++ 000-js/006-typescript/001-empezando/003-depuracion/src/index.ts
@@ -0,0 +1,4 @@
+let userName: string = "gilgamesh";
+userName = "ye wenjie";
+userName = "yun tianming";
+console.log(userName);
