00c8baf5de4313a214b6787629062eca1cbcb990
cesvh <cesar.vega.hdez@gmail.com>
2025-03-05 00:54:23 -0600

    fix: ts - init
    Configurando el archivo tsconfig.json
    
    Para que no haya error de Cannot redeclare block-scoped variable message. Esta es una alternativa a renombrar la variable.

--- 000-js/006-typescript/001-empezando/001-planck/001-planck.ts
+++ 000-js/006-typescript/001-empezando/001-planck/001-planck.ts
@@ -1,2 +1,4 @@
 let message: string = 'Hello, World!';
 console.log(message);
+
+export {}; // Para que no haya error de "Cannot redeclare block-scoped variable 'message'." Esta es una alternativa a renombrar la variable.
