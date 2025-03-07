045c61794106a53950eba16a97b2c37deec0a456
cesvh <cesar.vega.hdez@gmail.com>
2025-03-07 11:41:06 -0600

    feat: ts - number

---
+++ 000-js/006-typescript/002-tipos-basicos/002-number/001-number.ts
@@ -0,0 +1,5 @@
+let myNumber: number = 2;
+let myNumberFloat: number = 299792.458;
+
+console.log(myNumber);
+console.log(myNumberFloat);
---
+++ 000-js/006-typescript/002-tipos-basicos/002-number/___logs.sh
@@ -0,0 +1,25 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number> tsc --version
+Version 5.8.2
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number> ts-node --version
+v10.9.2
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number> ts-node .\001-number.ts
+2
+299792.458
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\002-tipos-basicos\002-number>
