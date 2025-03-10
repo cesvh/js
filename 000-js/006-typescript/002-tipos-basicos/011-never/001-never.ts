2fa370d00c85e560c3a80c2f6c5f3629135f7875
cesvh <cesar.vega.hdez@gmail.com>
2025-03-09 23:42:45 -0600

    feat: ts-never

---
+++ 000-js/006-typescript/002-tipos-basicos/011-never/001-never.ts
@@ -0,0 +1,21 @@
+type Role = 'admin' | 'user' | 'guest';
+
+const unknownRole = (role: never): never => {
+  throw new Error(`Invalid role: ${role}`);
+};
+
+const authorize = (role: Role): string => {
+  switch (role) {
+    case 'admin':
+      return 'You can do anything';
+    case 'user':
+      return 'You can do something';
+    case 'guest':
+      return 'You can do nothing';
+    default:
+      // never reach here util we add a new role
+      return unknownRole(role);
+  }
+};
+
+console.log(authorize('admin'));
