e4f4cc2471118a83edec15472ea047caf3b95b18
cesvh <cesar.vega.hdez@gmail.com>
2025-03-10 15:13:51 -0600

    feat: ts-alias

---
+++ 000-js/006-typescript/002-tipos-basicos/013-tipo-alias/000-casos-de-uso/001-tipo-alias/001-tipo-alias.js
@@ -0,0 +1,4 @@
+"use strict";
+let firstName = 'ces';
+let lastName = 'vh';
+console.log(`Mi nombre es ${firstName} ${lastName}`);
---
+++ 000-js/006-typescript/002-tipos-basicos/013-tipo-alias/000-casos-de-uso/001-tipo-alias/001-tipo-alias.ts
@@ -0,0 +1,6 @@
+type Name = string;
+
+let firstName: Name = 'ces';
+let lastName: Name = 'vh';
+
+console.log(`Mi nombre es ${firstName} ${lastName}`);
