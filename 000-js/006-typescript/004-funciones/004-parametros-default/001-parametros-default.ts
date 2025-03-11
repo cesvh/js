829eef3110e1cbbb1a5f9e3682f700f01053b446
cesvh <cesar.vega.hdez@gmail.com>
2025-03-11 14:29:25 -0600

    feat: Parámetros default

---
+++ 000-js/006-typescript/004-funciones/004-parametros-default/001-parametros-default.ts
@@ -0,0 +1,41 @@
+function aplicarDescuento(precio: number, descuento: number = 5): number {
+    let descuentoDecimal: number = descuento / 100;
+    return precio * (1 - descuentoDecimal);
+}
+
+console.log(aplicarDescuento(100));
+console.log(aplicarDescuento(100, 10));
+
+function obtenerDia(year: number = new Date().getFullYear(), month: number): number {
+    let day = 0;
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            day = 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            day = 30;
+            break;
+        case 2:
+            // leap year
+            if (((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0))
+                day = 29;
+            else
+                day = 28;
+            break;
+        default:
+            throw Error('Mes Inválido');
+    }
+    return day;
+}
+
+console.log(obtenerDia(2019, 2));
+console.log(obtenerDia(undefined, 2));
---
+++ 000-js/006-typescript/004-funciones/004-parametros-default/___logs.sh
@@ -0,0 +1,36 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default> ts-node .\001-parametros-default.ts
+95
+90
+28
+28
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default> tsc
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a---       11/03/2025 02:06 p. m.              0 ___logs.sh
+-a---       11/03/2025 02:27 p. m.            955 001-parametros-default.js
+-a---       11/03/2025 02:26 p. m.           1041 001-parametros-default.ts
+-a---       11/03/2025 02:26 p. m.          12926 tsconfig.json
+
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\004-parametros-default>
