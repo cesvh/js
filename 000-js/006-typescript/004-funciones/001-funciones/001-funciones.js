21edf29dee3063804e9a2132e3291d1451b71643
cesvh <cesar.vega.hdez@gmail.com>
2025-03-11 13:19:35 -0600

    feat: funciones

---
+++ 000-js/006-typescript/004-funciones/001-funciones/001-funciones.js
@@ -0,0 +1,10 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+let sum = add(10, 20);
+console.log(sum);
+function echo(message) {
+    console.log(message.toUpperCase());
+}
+echo('Hello World!');
---
+++ 000-js/006-typescript/004-funciones/001-funciones/___logs.sh
@@ -0,0 +1,34 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones> ts-node .\001-funciones.ts
+30
+HELLO WORLD!
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones> tsc
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a---       11/03/2025 12:48 p. m.              0 ___logs.sh
+-a---       11/03/2025 12:59 p. m.            185 001-funciones.js
+-a---       11/03/2025 12:55 p. m.            224 001-funciones.ts
+-a---       11/03/2025 12:56 p. m.          12926 tsconfig.json
+
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\004-funciones\001-funciones>
