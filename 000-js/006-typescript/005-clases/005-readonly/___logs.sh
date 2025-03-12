054ccf6bc4a89116ce88d9d92109d2ddc3b22dd2
cesvh <cesar.vega.hdez@gmail.com>
2025-03-12 16:37:52 -0600

    fix: ts - readonly

---
+++ 000-js/006-typescript/005-clases/005-readonly/___logs.sh
@@ -0,0 +1,33 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly> ts-node .\001-readonly.ts
+1991-01-25T06:00:00.000Z
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly> tsc
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a---       12/03/2025 04:27 p. m.              0 ___logs.sh
+-a---       12/03/2025 04:34 p. m.            273 001-readonly.js
+-a---       12/03/2025 04:33 p. m.            308 001-readonly.ts
+-a---       12/03/2025 04:26 p. m.          12926 tsconfig.json
+
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\005-clases\005-readonly>
