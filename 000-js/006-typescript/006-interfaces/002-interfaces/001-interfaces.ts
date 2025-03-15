323757f8d4be1363d5123c89d98c0924aa41e514
cesvh <cesar.vega.hdez@gmail.com>
2025-03-15 01:48:04 -0600

    feat: Interfaces

---
+++ 000-js/006-typescript/006-interfaces/002-interfaces/001-interfaces.ts
@@ -0,0 +1,23 @@
+// Por convención, los nombres de las interfaces se escriben en PascalCase.
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+// Tras definir la interfaz Person, puede usarse como tipo. Por ejemplo, se puede anotar el parámetro de función con el nombre de la interfaz:
+// function getFullName(person: Person) {
+//     return `${person.firstName} ${person.lastName}`;
+// }
+
+function getFullName({ firstName, lastName}: Person) {
+    return `${firstName} ${lastName}`;
+}
+
+// La función getFullName() aceptará cualquier objeto que tenga al menos dos propiedades de cadena con el nombre firstName y lastName.
+let cesar = {
+    firstName: 'César',
+    lastName: 'Vega',
+    year: 1991
+};
+
+console.log(getFullName(cesar)); // César Vega
---
+++ 000-js/006-typescript/006-interfaces/002-interfaces/___logs.sh
@@ -0,0 +1,33 @@
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces> tsc --init
+
+Created a new tsconfig.json with:
+                                                                                                                     TS
+  target: es2016
+  module: commonjs
+  strict: true
+  esModuleInterop: true
+  skipLibCheck: true
+  forceConsistentCasingInFileNames: true
+
+
+You can learn more at https://aka.ms/tsconfig
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces> ts-node .\001-interfaces.ts
+César Vega
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces> tsc
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a---       15/03/2025 01:38 a. m.              0 ___logs.sh
+-a---       15/03/2025 01:46 a. m.            615 001-interfaces.js
+-a---       15/03/2025 01:46 a. m.            778 001-interfaces.ts
+-a---       15/03/2025 01:39 a. m.          12926 tsconfig.json
+
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces>
+PS C:\Users\cesvh\dev\github\js\000-js\006-typescript\006-interfaces\002-interfaces>
