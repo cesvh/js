fbb88c7d671037eb7344f0f9b310e744f899b75a
cesvh <cesar.vega.hdez@gmail.com>
2025-03-23 01:51:53 -0600

    fix: nextjs-ts Separaciones de componentes Función expresión
    Código más legible y escalable

--- 004-nextjs/100-projects/003-next-ts/001-create-ts/src/app/page.tsx
+++ 004-nextjs/100-projects/003-next-ts/001-create-ts/src/app/page.tsx
@@ -4,37 +4,14 @@ import Fundamentos from "@/components/buttons/Fundamentos";
 import PropsTipados from "@/components/buttons/PropsTipados";
 import Css from "@/components/buttons/Css";
 import RecordType from "@/components/buttons/RecordType";
-
-type Color = "red" | "blue" | "green";
-type ButtonProps = {
-  text: string;
-  subtitle: string;
-  color: Color;
-  backgroundColor?: Color;
-  sizes?: 30 | 40 | 50;
-  padding?: [number, number, number?, number?];
-}
-
-const Button3 = (props: ButtonProps) => {
-  return (
-    <button>ArrayUnionType {props.text} - {props.subtitle} - {props.color}</button>
-  )
-}
-
-const Button4: React.FC<ButtonProps> = (props) => {
-  const {text, subtitle} = props;
-  return (
-    <button>ArrayUnionType {text} - {subtitle}</button>
-  )
-}
+import FuncionExpresion from "@/components/buttons/FuncionExpresion";
 
 function Page() {
   return (
     <div>
       <Fundamentos text="Texto props" /><br /><br />
       <PropsTipados text="Texto props" subtitle="Subtítulo" color="red" /><br /><br />
-      <Button3 text="Texto props" subtitle="Subtítulo" color="green" /><br /><br />
-      <Button4 text="Texto props" subtitle="Subtítulo" color="green" padding={[22, 23]} /><br /><br />
+      <FuncionExpresion /><br /><br />
       <Css style={{
         color: "black",
         backgroundColor: "gold",
---
+++ 004-nextjs/100-projects/003-next-ts/001-create-ts/src/components/buttons/FuncionExpresion.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type Color = "red" | "blue" | "green";
+type ButtonProps = {
+  text: string;
+  subtitle: string;
+  color: Color;
+  backgroundColor?: Color;
+  sizes?: 30 | 40 | 50;
+  padding?: [number, number, number?, number?];
+}
+
+const Button3 = (props: ButtonProps) => {
+  return (
+    <button>ArrayUnionType {props.text} - {props.subtitle} - {props.color}</button>
+  )
+}
+
+const Button4: React.FC<ButtonProps> = (props) => {
+  const {text, subtitle} = props;
+  return (
+    <button>ArrayUnionType {text} - {subtitle}</button>
+  )
+}
+
+function FuncionExpresion() {
+  return (
+    <>
+        <Button3 text="Texto props" subtitle="Subtítulo" color="green" /><br /><br />
+        <Button4 text="Texto props" subtitle="Subtítulo" color="green" padding={[22, 23]} /><br /><br />
+    </>
+  )
+}
+
+export default FuncionExpresion
