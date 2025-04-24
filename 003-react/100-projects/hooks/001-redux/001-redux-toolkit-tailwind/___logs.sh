e8c9b4eb556cc12f19384e78cf8bbc2ef2e020e1
cesvh <cesar.vega.hdez@gmail.com>
2025-04-24 13:10:17 -0600

    feat: js react redux Creación de proyectos con redux y redux toolkit
    Pequeño crud con tailwindcss

---
+++ 003-react/100-projects/hooks/001-redux/001-redux-toolkit-tailwind/___logs.sh
@@ -0,0 +1,73 @@
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux> npm create vite
+√ Project name: ... 001-redux-toolkit-tailwind
+√ Select a framework: » React
+√ Select a variant: » JavaScript
+
+Scaffolding project in C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind...
+
+Done. Now run:
+
+  cd 001-redux-toolkit-tailwind
+  npm install
+  npm run dev
+
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux> cd .\001-redux-toolkit-tailwind\
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind> ls
+
+    Directory: C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       24/04/2025 01:03 p. m.                public
+d----       24/04/2025 01:03 p. m.                src
+-a---       02/11/2023 08:18 p. m.            526 .eslintrc.cjs
+-a---       02/11/2023 08:18 p. m.            253 .gitignore
+-a---       02/11/2023 08:18 p. m.            361 index.html
+-a---       24/04/2025 01:03 p. m.            670 package.json
+-a---       02/11/2023 08:18 p. m.            451 README.md
+-a---       02/11/2023 08:18 p. m.            163 vite.config.js
+
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind> npm run dev
+
+> 001-redux-toolkit-tailwind@0.0.0 dev
+> vite
+
+"vite" no se reconoce como un comando interno o externo,
+programa o archivo por lotes ejecutable.
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind> npm install
+npm WARN deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+npm WARN deprecated @humanwhocodes/config-array@0.13.0: Use @eslint/config-array instead
+npm WARN deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
+npm WARN deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+npm WARN deprecated @humanwhocodes/object-schema@2.0.3: Use @eslint/object-schema instead
+npm WARN deprecated eslint@8.57.1: This version is no longer supported. Please see https://eslint.org/version-support for other options.
+
+added 272 packages, and audited 273 packages in 18s
+
+109 packages are looking for funding
+  run `npm fund` for details
+
+2 moderate severity vulnerabilities
+
+To address all issues (including breaking changes), run:
+  npm audit fix --force
+
+Run `npm audit` for details.
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind>
+PS C:\Users\cesvh\dev\github\js\003-react\100-projects\hooks\001-redux\001-redux-toolkit-tailwind> npm run dev
+
+> 001-redux-toolkit-tailwind@0.0.0 dev
+> vite
+
+
+  VITE v4.5.13  ready in 345 ms
+
+  ➜  Local:   http://localhost:5173/
+  ➜  Network: use --host to expose
+  ➜  press h to show help
