4cca1f6647f5a793f0c9561f280dbf15261d5e5c
cesvh <cesar.vega.hdez@gmail.com>
2026-01-01 20:07:00 -0600

    feat: js, array, push, unshift

--- ___otros/000-metalcode.io/000-fundamentos-de-js/002-array/002-unshift.js
+++ ___otros/000-metalcode.io/000-fundamentos-de-js/002-array/002-unshift.js
@@ -2,15 +2,14 @@ const arrayCadena = ["Lao", "Tse", "Asimov", "Yun", "Tianming"];
 
 console.log(`arrayCadena: ${arrayCadena}`);
 
-arrayCadena.push("Gibson");
+arrayCadena.unshift("Gibson");
 
 console.log(`arrayCadena: ${arrayCadena}`);
 
 /*
 PS C:\Users\cesvh\dev\github\js>
-PS C:\Users\cesvh\dev\github\js> node ___otros\000-metalcode.io\000-fundamentos-de-js\002-array\001-push.js
+PS C:\Users\cesvh\dev\github\js> node ___otros\000-metalcode.io\000-fundamentos-de-js\002-array\002-unshift.js
 arrayCadena: Lao,Tse,Asimov,Yun,Tianming
-arrayCadena: Lao,Tse,Asimov,Yun,Tianming,Gibson
-PS C:\Users\cesvh\dev\github\js>
+arrayCadena: Gibson,Lao,Tse,Asimov,Yun,Tianming
 PS C:\Users\cesvh\dev\github\js>
 */
