4edaf1533f5083a69e1adabae650a9d6eb1b0c4d
cesvh <cesar.vega.hdez@gmail.com>
2026-01-01 19:57:06 -0600

    feat: js, array, push

---
+++ ___otros/000-metalcode.io/000-fundamentos-de-js/002-array/001-push.js
@@ -0,0 +1,7 @@
+const arrayCadena = ["Lao", "Tse", "Asimov", "Yun", "Tianming"];
+
+console.log(`arrayCadena: ${arrayCadena}`);
+
+arrayCadena.push("Gibson");
+
+console.log(`arrayCadena: ${arrayCadena}`);
