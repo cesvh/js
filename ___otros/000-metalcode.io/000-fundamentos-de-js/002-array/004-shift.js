3a0a5217121c35e9c3d4ef2ca5c9662b4249e057
cesvh <cesar.vega.hdez@gmail.com>
2026-01-01 20:16:24 -0600

    feat: js, array, shift

---
+++ ___otros/000-metalcode.io/000-fundamentos-de-js/002-array/004-shift.js
@@ -0,0 +1,20 @@
+const arrayCadena = ["Lao", "Tse", "Asimov", "Yun", "Tianming"];
+
+console.log(`arrayCadena: ${arrayCadena}`);
+
+const itemEliminado = arrayCadena.shift();
+
+console.log(`itemEliminado: ${itemEliminado}`);
+console.log(`arrayCadena: ${arrayCadena}`);
+
+/*
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js> node ___otros\000-metalcode.io\000-fundamentos-de-js\002-array\004-shift.js
+arrayCadena: Lao,Tse,Asimov,Yun,Tianming
+itemEliminado: Lao
+arrayCadena: Tse,Asimov,Yun,Tianming
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+*/
