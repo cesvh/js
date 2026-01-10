32722cde1e080d0fb7e0d55ab477a47ce50d4801
cesvh <cesar.vega.hdez@gmail.com>
2026-01-09 22:39:42 -0600

    feat: js, spread

---
+++ ___otros/000-metalcode.io/000-fundamentos-de-js/003-referencia-mutabilidad/004-spread.js
@@ -0,0 +1,59 @@
+const objetoLibro1 = { nombre: "neuromante", precio: 500 };
+const objetoLibro2 = { ...objetoLibro1 };
+
+// console.log("\nobjetoLibro1:");
+console.log(objetoLibro1);
+// console.log("\nobjetoLibro2:");
+console.log(objetoLibro2);
+// console.log("\nobjetoLibro1 === objetoLibro2:");
+console.log(objetoLibro1 === objetoLibro2);
+objetoLibro2.precio = 200;
+// console.log("\nobjetoLibro2:");
+console.log(objetoLibro2);
+
+console.log("\n------------------------------------------------");
+
+const objetoLibro3 = { ...objetoLibro1, precio: 300 };
+// console.log("\nobjetoLibro3:");
+console.log(objetoLibro3);
+// console.log("\nobjetoLibro1 === objetoLibro3:");
+console.log(objetoLibro1 === objetoLibro3);
+
+console.log("\n------------------------------------------------");
+
+function toUpperCaseImmutable(libro) {
+    const nuevoLibro = {
+        ...libro,
+        nombre: libro.nombre.toUpperCase()
+    };
+    return nuevoLibro;
+}
+
+const objetoLibro4 = toUpperCaseImmutable(objetoLibro1);
+console.log(objetoLibro1);
+console.log(objetoLibro4);
+console.log(objetoLibro1 === objetoLibro4);
+
+/*
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js> node ___otros\000-metalcode.io\000-fundamentos-de-js\003-referencia-mutabilidad\004-spread.js
+{ nombre: 'neuromante', precio: 500 }
+{ nombre: 'neuromante', precio: 500 }
+false
+{ nombre: 'neuromante', precio: 200 }
+
+------------------------------------------------
+{ nombre: 'neuromante', precio: 300 }
+false
+
+------------------------------------------------
+{ nombre: 'neuromante', precio: 500 }
+{ nombre: 'NEUROMANTE', precio: 500 }
+false
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+PS C:\Users\cesvh\dev\github\js>
+*/
--- ___otros/000-metalcode.io/000-fundamentos-de-js/fundamentos-js-hdleon.js
+++ ___otros/000-metalcode.io/000-fundamentos-de-js/fundamentos-js-hdleon.js
@@ -66,7 +66,7 @@ Referencia, Mutabilidad e Inmutabilidad
 	// 36 (26.67%) - 100 - Paso por Valor y Referencia como parámetro en funciones
 	// 37 (27.41%) - 99 - Mutabilidad en Programación
 	// 38 (28.15%) - 98 - Funciones Inmutables 
-	39 (28.89%) - 97 - Operador Spread (...)
+	// 39 (28.89%) - 97 - Operador Spread (...)
 	40 (29.63%) - 96 - Operador Spread en Arrays
 	41 (30.37%) - 95 - Operador Spread para combinar objetos
 	42 (31.11%) - 94 - Clonación de objetos a profundidad con structureClone()
